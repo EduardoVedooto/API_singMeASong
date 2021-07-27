14801b8041e7658005404a7367887b3bf55097f2
thiagoribb <thiribeiro142@gmail.com>
2021-07-27 15:45:42 -0300

    preparando review

--- src/routes/genresRouter.ts
+++
@@ -1,9 +0,0 @@
-import * as genresController from "../controllers/genresController";
-import express from "express";
-
-const router = express.Router();
-
-router.post("/genres", genresController.create);
-router.get("/genres", genresController.read);
-
-export { router as genresRouter };
--- tests/utils/wipeTable.ts
+++
@@ -1,7 +0,0 @@
-import connection from "../../src/database"
-
-const wipeTable = async (table: string) => {
-  await connection.query(`TRUNCATE TABLE ${table} RESTART IDENTITY CASCADE`);
-};
-
-export default wipeTable;
